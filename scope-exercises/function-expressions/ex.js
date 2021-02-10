c6f41ba488dd8d46b6720a4b1915f7215fcbcb02
Denny Hong <hong961127@gmail.com>
2021-02-09 22:08:05 -0800

    Add function expression exercise solution

--- scope-exercises/function-expressions/ex.js
+++ scope-exercises/function-expressions/ex.js
@@ -1,38 +1,47 @@
-function printRecords(recordIds) {
-	// TODO
-}
-
-function paidStudentsToEnroll() {
-	// TODO
-}
-
-function remindUnpaid(recordIds) {
-	// TODO
-}
-
-
 // ********************************
+const printRecords = (recordIds) =>
+	studentRecords
+		.filter((record) => recordIds.includes(record.id))
+		.sort((recordA, recordB) =>
+			recordA.name < recordB.name ? -1 : recordA.name > recordB.name ? 1 : 0
+		)
+		.map((record) => `${record.name} (${record.id}): ${record.paid ? "Paid" : "Not Paid"}`)
+		.join("\n");
 
-var currentEnrollment = [ 410, 105, 664, 375 ];
-
-var studentRecords = [
-	{ id: 313, name: "Frank", paid: true, },
-	{ id: 410, name: "Suzy", paid: true, },
-	{ id: 709, name: "Brian", paid: false, },
-	{ id: 105, name: "Henry", paid: false, },
-	{ id: 502, name: "Mary", paid: true, },
-	{ id: 664, name: "Bob", paid: false, },
-	{ id: 250, name: "Peter", paid: true, },
-	{ id: 375, name: "Sarah", paid: true, },
-	{ id: 867, name: "Greg", paid: false, },
+const paidStudentsToEnroll = () => [
+	...currentEnrollment,
+	...studentRecords
+		.filter((record) => !currentEnrollment.includes(record.id) && record.paid)
+		.map((record) => record.id),
 ];
 
-printRecords(currentEnrollment);
+const remindUnpaid = (recordIds) =>
+	printRecords(
+		studentRecords
+			.filter((record) => recordIds.includes(record.id) && !record.paid)
+			.map((record) => record.id)
+	);
+
+let currentEnrollment = [410, 105, 664, 375];
+
+const studentRecords = [
+	{ id: 313, name: "Frank", paid: true },
+	{ id: 410, name: "Suzy", paid: true },
+	{ id: 709, name: "Brian", paid: false },
+	{ id: 105, name: "Henry", paid: false },
+	{ id: 502, name: "Mary", paid: true },
+	{ id: 664, name: "Bob", paid: false },
+	{ id: 250, name: "Peter", paid: true },
+	{ id: 375, name: "Sarah", paid: true },
+	{ id: 867, name: "Greg", paid: false },
+];
+
+console.log(printRecords(currentEnrollment));
 console.log("----");
 currentEnrollment = paidStudentsToEnroll();
-printRecords(currentEnrollment);
+console.log(printRecords(currentEnrollment));
 console.log("----");
-remindUnpaid(currentEnrollment);
+console.log(remindUnpaid(currentEnrollment));
 
 /*
 	Bob (664): Not Paid
